b51ce560e09c045d338951ededa4b35dbe74230b
Matheus Barbosa <matheuskleber@live.com>
2022-10-08 14:34:56 -0300

    feat(error): error handler middleware

---
+++ api/middleware/error_middleware.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/matheusrbarbosa/gofin/domain/exceptions"
+)
+
+func ErrorHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+		for _, e := range c.Errors {
+			err := e.Err
+			if gErr, ok := err.(*exceptions.GofinError); ok {
+				c.JSON(gErr.Code, gin.H{
+					"code":        gErr.Code,
+					"message":     gErr.Message,
+					"description": gErr.Description,
+				})
+			} else {
+				c.JSON(http.StatusOK, gin.H{
+					"code": 500, "msg": "Server exception", "data": err.Error(),
+				})
+			}
+		}
+	}
+}
